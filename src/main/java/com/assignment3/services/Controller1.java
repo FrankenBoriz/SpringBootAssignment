1fa4f98611d9dcddbd4eaa517ccb835c46648d06
Boris Ilievski <Boris Ilievski>
2017-03-17 11:20:27 +0100

    Initial commit

---
+++ src/main/java/com/assignment3/services/Controller1.java
@@ -0,0 +1,21 @@
+package com.assignment3.services;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * Created by Boris on 17.03.2017.
+ */
+@Controller
+@EnableAutoConfiguration
+public class Controller1 {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World";
+    }
+}
