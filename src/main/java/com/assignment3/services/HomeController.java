95be9579a803dec3920e215bc5810bf623a2d4ab
ArgTang <ArgTang@outlook.com>
2017-03-23 17:04:13 +0100

    Add index, Greeting

--- src/main/java/com/assignment3/services/HomeController.java
+++ src/main/java/com/assignment3/services/HomeController.java
@@ -2,20 +2,17 @@ package com.assignment3.services;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
-/**
- * Created by Boris on 17.03.2017.
- */
 @Controller
 @EnableAutoConfiguration
 public class HomeController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home(){
-        return "Hello World";
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
