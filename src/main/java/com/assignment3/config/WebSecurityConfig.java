47c9c770414daaa9298233732115b952b3da44a0
ArgTang <ArgTang@outlook.com>
2017-03-24 11:02:47 +0100

    Redirec to myprofile

--- src/main/java/com/assignment3/config/WebSecurityConfig.java
+++ src/main/java/com/assignment3/config/WebSecurityConfig.java
@@ -18,6 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .formLogin()
                     .loginPage("/login")
+                    .successForwardUrl("/myprofile")
                     .permitAll()
                     .and()
                 .logout()
